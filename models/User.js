33f57091d5ac48bb200eac47ac4c639eb330c087
Hasan Mahfoodh <hasoony201@gmail.com>
2026-04-17 09:14:35 +0300

    create user model + create user router

--- models/User.js
+++ models/User.js
@@ -1,1 +1,16 @@
 //User model
+const { Schema } = require("mongoose")
+
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: false },
+    image: { type: String, required: false },
+    phoneNum: { type: String, required: false },
+    activeReserve: { type: Boolean, required: false },
+  },
+  { timestamps: true }
+)
+
+module.exports = userSchema
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,1 +1,9 @@
 // auth router
+const router = require("express").Router()
+const controller = require("../controllers/authController")
+
+router.get("/test", (req, res) => {
+  res.send("working")
+})
+
+module.exports = router
