0e38e198b5d78d72ab04e98230b891fa66ab4a5f
Hasan Mahfoodh <hasoony201@gmail.com>
2026-04-19 15:53:52 +0300

    general fixes

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,1 +1,8 @@
 // user router
+const router = require("express").Router()
+const controller = require("../controllers/userController")
+const middleware = require("../middleware")
+
+router.get("/purchase-history/:id", controller.getPurchaseHistory)
+
+module.exports = router
