1e1e3f100a90643e7a4bd1062ecec9898e26264d
Aboood105 <orcs882@gmail.com>
2026-04-20 09:15:37 +0300

    fix

--- db/index.js
+++ db/index.js
@@ -1,8 +1,7 @@
-const mongoose = require("mongoose")
-require("dotenv").config()
-
 const dns = require("dns")
 dns.setServers(["8.8.8.8", "1.1.1.1"])
+const mongoose = require("mongoose")
+require("dotenv").config()
 
 mongoose.set("strictQuery", false)
 
